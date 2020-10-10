57d151707b5811aed75dd7b3651b1a587d84e1ef
swiftcode <mpc@junct.com>
2020-10-09 23:27:45 -0500

    Initial Commit

---
+++ Configuration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Configuration
+//
+//  Created by mpc on 10/9/20.
+//  Copyright © 2020 mpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
